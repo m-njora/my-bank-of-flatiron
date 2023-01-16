e0bd862c6575e22a66262e230bc1f485316eef52
m-njora <mmuthekijora@gmail.com>
2023-01-16 21:13:06 +0300

    added transaction component

--- bank-of-flatiron/src/components/App.js
+++ bank-of-flatiron/src/components/App.js
@@ -1,30 +1,45 @@
 import React from "react";
-import ExpenseForm from "./ExpenseForm";
-import ExpenseList from "./ExpenseList";
+// import { BasicTable } from "./BasicTable";
+// import ExpenseForm from "./ExpenseForm";
+// import ExpenseList from "./ExpenseList";
+import AccountContainer from "./AccountContainer";
+
 //import "bootstrap/dist/css/bootstrap"
 //import  './App.css';
-
 function App() {
-    return(
-        <div className="container">
-            <h1>My Bank  </h1>
-            <div>
-                <h3 className="mt-3">Expenses</h3>
-            </div>
-
-            <div className="col-sm">
-                <ExpenseList/>
-            </div>
-            <h3 className="mt-3">Add Transaction</h3>
-            <div className="mt-3">
-                <div className="column">
-                <ExpenseForm/>
-                </div>
-            </div>
+    return (
+      <div className="ui raised segment">
+        <div className="ui segment violet inverted">
+          <h2>The Royal Bank of Flatiron</h2>
         </div>
+        <AccountContainer />
+      </div>
+    );
+  }
+// function App() {
+//     return(
+//         <div className="container">
+//             <h1>My Bank  </h1>
+//             <div>
+//                 <h3 className="mt-3">Expenses</h3>
+//             </div>
+//             <div>
+//              <BasicTable/>
+//             </div>
+            
+//             <div className="col-sm">
+//                 <ExpenseList/>
+//             </div>
+//             <h3 className="mt-3">Add Transaction</h3>
+//             <div className="mt-3">
+//                 <div className="column">
+//                 <ExpenseForm/>
+//                 </div>
+//             </div>
+//         </div>
         
-    )
+//     )
     
-}
+// }
 
 export default App;
