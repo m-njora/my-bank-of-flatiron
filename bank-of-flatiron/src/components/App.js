cb6f9d010b1967d1c74a6f03e951b607fe6500ce
m-njora <mmuthekijora@gmail.com>
2023-01-16 21:35:27 +0300

    updated app js

--- bank-of-flatiron/src/components/App.js
+++ bank-of-flatiron/src/components/App.js
@@ -1,45 +1,15 @@
 import React from "react";
-// import { BasicTable } from "./BasicTable";
-// import ExpenseForm from "./ExpenseForm";
-// import ExpenseList from "./ExpenseList";
 import AccountContainer from "./AccountContainer";
 
-//import "bootstrap/dist/css/bootstrap"
-//import  './App.css';
 function App() {
-    return (
-      <div className="ui raised segment">
-        <div className="ui segment violet inverted">
-          <h2>The Royal Bank of Flatiron</h2>
-        </div>
-        <AccountContainer />
+  return (
+    <div className="ui raised segment">
+      <div className="ui segment violet inverted">
+        <h2>The Royal Bank of Flatiron</h2>
       </div>
-    );
-  }
-// function App() {
-//     return(
-//         <div className="container">
-//             <h1>My Bank  </h1>
-//             <div>
-//                 <h3 className="mt-3">Expenses</h3>
-//             </div>
-//             <div>
-//              <BasicTable/>
-//             </div>
-            
-//             <div className="col-sm">
-//                 <ExpenseList/>
-//             </div>
-//             <h3 className="mt-3">Add Transaction</h3>
-//             <div className="mt-3">
-//                 <div className="column">
-//                 <ExpenseForm/>
-//                 </div>
-//             </div>
-//         </div>
-        
-//     )
-    
-// }
+      <AccountContainer />
+    </div>
+  );
+}
 
 export default App;
