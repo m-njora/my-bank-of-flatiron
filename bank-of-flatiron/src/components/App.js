2461549d7f4d5588da8de87c49fc76755318f15d
m-njora <mmuthekijora@gmail.com>
2023-01-17 08:20:27 +0300

    updated readme

--- bank-of-flatiron/src/components/App.js
+++ bank-of-flatiron/src/components/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="ui raised segment">
       <div className="ui segment violet inverted">
-        <h2>The Royal Bank of Flatiron</h2>
+        <h2>The Bank of Flatiron</h2>
       </div>
       <AccountContainer />
     </div>
